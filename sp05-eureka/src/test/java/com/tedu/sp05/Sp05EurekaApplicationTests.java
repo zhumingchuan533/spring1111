7554323c3f88f12218bb132715b1bcd3b3ce9fa7
zhumingchuan533 <56984712+zhumingchuan533@users.noreply.github.com>
2019-11-01 14:17:04 +0800

    555

---
+++ sp05-eureka/src/test/java/com/tedu/sp05/Sp05EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tedu.sp05;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Sp05EurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("sasa");
+	}
+
+}
---
+++ sp05-eureka/src/test/java/com/tedu/sp05/Testsss.java
@@ -0,0 +1,10 @@
+package com.tedu.sp05;
+
+import org.junit.Test;
+
+public class Testsss {
+	@Test
+  public void Tsasa() {
+	  System.out.println("nsasansnasn");
+  }
+}
