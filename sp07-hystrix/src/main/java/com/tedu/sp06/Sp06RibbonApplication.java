4f5c829b662e3a6f3a5befbffb07608da485fc2f
zhumingchuan533 <56984712+zhumingchuan533@users.noreply.github.com>
2019-11-01 14:19:38 +0800

    777

---
+++ sp07-hystrix/src/main/java/com/tedu/sp06/Sp06RibbonApplication.java
@@ -0,0 +1,31 @@
+package com.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+//@EnableDiscoveryClient
+//@SpringBootApplication
+//@EnableCircuitBreaker
+@SpringCloudApplication//三合一
+public class Sp06RibbonApplication {
+	//创建RestTemplate实例,存入spring容器
+	@LoadBalanced //负载均衡
+	@Bean
+	public RestTemplate getRestTemplate() {
+		SimpleClientHttpRequestFactory f = new SimpleClientHttpRequestFactory();
+		f.setConnectTimeout(1000);//连接一秒超时
+		f.setReadTimeout(1000);
+		return new RestTemplate(f);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp06RibbonApplication.class, args);
+	}
+
+}
